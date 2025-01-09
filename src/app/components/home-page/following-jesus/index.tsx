d19ad449d8bbc319c93e11a8f81d05732e1b7e72
kvngOluchi <150791203+kvngOluchi@users.noreply.github.com>
2025-01-09 02:12:27 -0800

    Update index.tsx

--- src/app/components/home-page/following-jesus/index.tsx
+++ src/app/components/home-page/following-jesus/index.tsx
@@ -5,7 +5,6 @@ export default function FollowingJesus() {
   return (
     <section className=" layout-spacing py-9 mt-2 px-1">
       <div className="custom-container grid md:grid-cols-[1fr_1fr]">
-        <div className="space-y-2 ">
           <span className="">
             <Image
               className="shadow-xl p-4 border-radius rounded-2xl"
@@ -15,7 +14,6 @@ export default function FollowingJesus() {
               height={0}
             />
           </span>
-        </div>
         <div className=" py-5 px-5">
           <h2 className=" font-bold text-xl ">
             Following Jesus Wherever we are
@@ -24,22 +22,16 @@ export default function FollowingJesus() {
           <p>
             We are excited to welcome you to our mission from all over the world
             we intend on sharing inspiring stories of faith and service through
-            our work together.
-          </p>
-          <p>
-            We are going to share testimonies from our work through christ
-            jesus.
-          </p>
-          <p>
-            By the Grace of God there will be opportunity to support our
+            our work together.We are going to share testimonies from our work through christ
+            jesus. By the Grace of God there will be opportunity to support our
             missionaries through prayer and giving
           </p>
-          <div className="flex gap-4 justify-center mt-10">
+          <div className="flex gap-1 justify-center lg:mx-24 lg:my4 my-2">
             <span>
               <Image
                 className="shadow-xl p-2 border-radius rounded-lg"
                 src={"/./image/donations-picture.jpg"}
-                width={229}
+                width={129}
                 height={0}
                 alt="donations"
               />
@@ -48,7 +40,7 @@ export default function FollowingJesus() {
               <Image
                 className="shadow-xl p-2 border-radius rounded-lg"
                 src={"/./image/bible-picture.jpg"}
-                width={229}
+                width={129}
                 height={0}
                 alt="donations"
               />
@@ -57,7 +49,7 @@ export default function FollowingJesus() {
               <Image
                 className="shadow-xl p-2 border-radius rounded-lg"
                 src={"/./image/funds-backky.jpg"}
-                width={229}
+                width={129}
                 height={0}
                 alt="donations"
               />
