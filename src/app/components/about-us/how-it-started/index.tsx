479b0b1efa33dc300941fbae071665383ccf0e0a
unknown <olumarvel1@gmail.com>
2025-01-12 09:26:09 -0800

    another one

--- src/app/components/about-us/how-it-started/index.tsx
+++ src/app/components/about-us/how-it-started/index.tsx
@@ -4,7 +4,7 @@ import ReadMore from "@/ui/read-more";
 
 export default function HowItStarted() {
   const str =
-    " The Lord asked, if you were a christian, what could you have done? I responded, i could have preached to her and peradventure she could have accepted Christ as her Lord Saviour and possibly make it to heaven, And the Lord said, Go and do likewise. Immediately, the burden came on me to go and rescue multitudes of souls that are passing through the hospitals into eternal damnation on daily basis.This is the beginning of my calling into hospital Ministry and the first tract that i wrote titled I watched her die came from this encounter with the Lord";
+    " The Lord asked, if you were a christian, what could you have done? I responded, I could have preached to her and peradventure she could have accepted Christ as her Lord Saviour and possibly make it to heaven, And the Lord said, Go and do likewise. Immediately, the burden came on me to go and rescue multitudes of souls that are passing through the hospitals into eternal damnation on daily basis.This is the beginning of my calling into hospital Ministry and the first tract that I wrote titled I watched her die came from this encounter with the Lord";
 
   return (
     <section className="layout-spacing p-5">
@@ -12,24 +12,24 @@ export default function HowItStarted() {
         <div>
           <h2 className=" font-bold text-xl">HOW IT STARTED</h2>
           <div className="w-20 h-1 bg-[#423e73] "></div>
-          <p className="pt-1">
+          <p className="pt-3">
             I was admitted into College Of Nursing Science in April 1983 and
             graduated in June 1986. As a student and an unbeliever in 1984,
             there was a middle aged woman that was admitted into the Hospital
             ward with chronic cancer of the cervix and she later died after much
             battles to save her life.
           </p>
-          <p className="pt-1">
+          <p className="pt-3">
             Barely a year after I graduated, I had an encounter with the Lord
             Jesus Christ on Saturday 11th April 1987 and became born again
             Christain during a revival meeting.
           </p>
-          <p className="pt-1">
+          <p className="pt-3">
             In 1994 exactly 10years later after the death of the cancer patient
             in 1984, the Lord appeared to me in a dream. He asked me if I could
             still remember the woman that was admitted into the female Surgical
             Ward with terminal disease of cancer of the cervix. The picture of
-            the woman appeared to me and i said yes, Lord. He said, I was aware
+            the woman appeared to me and I said yes, Lord. He said, I was aware
             of your emotional attachment to her and how you were always standing
             by her bedside wondering the kind of pains she was experiencing.
           </p>
