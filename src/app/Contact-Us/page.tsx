4fce0b1b1606c5bd36741159ae5cc5e4335e39ab
kvngOluchi <150791203+kvngOluchi@users.noreply.github.com>
2025-01-12 04:12:28 -0800

    Update page.tsx

--- src/app/Contact-Us/page.tsx
+++ src/app/Contact-Us/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ContactUsDefinition from "../components/contact-us/contact-us-hero";
 import ContactDet from "../components/contact-us/contact-det";
-import { main } from "framer-motion/client";
 import ContactUsHero from "../components/contact-us/contact-us-hero";
 
 export default function page() {
